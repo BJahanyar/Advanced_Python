0c6d5c7fe63dea0e385f3e51e42f2bb2df0228e0
Bahar Jahanyar <49306136+BJahanyar@users.noreply.github.com>
2021-06-13 11:48:16 +0430

    Add files via upload

---
+++ Image_Processing/Session_21/EX_1st.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+############################
+
+rows = 800
+cols = 800
+
+chess = np.ones((rows, cols), np.uint8) * 255
+
+for i in range (rows):
+    for j in range (cols):
+        if (i // 100) % 2 == 1:
+            if (j//100) % 2 == 0:
+             chess [ i , j ] = 0
+        else:
+            if (j//100) % 2 == 1:
+             chess [ i , j ] = 0
+
+cv2.imshow('Created Chess',chess)
+cv2.waitKey()
