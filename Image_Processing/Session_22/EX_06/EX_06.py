64cec828018ee4f2d1e647ff0c95ec6bc603012f
Bahar Jahanyar - Surface <B.Jahanyar@Outlook.com>
2021-06-20 12:53:21 +0430

    final

--- Image_Processing/Session_22/EX_06/EX_06.py
+++ Image_Processing/Session_22/EX_06/EX_06.py
@@ -10,10 +10,10 @@ image = cv2.cvtColor (image , cv2.COLOR_BGR2GRAY)
 def get_gradient_2d(start, stop, width, height):
     return np.tile(np.linspace(start, stop, width), (height, 1))
 
-gradient = get_gradient_2d(0,  255, cols, rows)
+gradient = get_gradient_2d(10,  255, cols, rows)
 gradient = np.uint8(gradient)
 
-mona_final = (image / gradient) 
+mona_final = (image / gradient)
 
 cv2.imshow('out', mona_final) 
 cv2.waitKey()
