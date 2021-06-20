d309056d03f0602f10775975c1d3667e320756c9
Bahar Jahanyar - Surface <B.Jahanyar@Outlook.com>
2021-06-20 11:10:59 +0430

    Final_Assign_22

--- Image_Processing/Session_22/EX_06/EX_06.py
+++ Image_Processing/Session_22/EX_06/EX_06.py
@@ -1,9 +1,20 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-img = cv2.imread ('mona_lisa.jpg')
-img = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
+image = cv2.imread ('mona_lisa.jpg') 
+image = cv2.cvtColor (image , cv2.COLOR_BGR2GRAY)
 
-final = 
+(rows , cols) = image.shape
 
-cv2.imshow('out',final)
+def get_gradient_2d(start, stop, width, height):
+    return np.tile(np.linspace(start, stop, width), (height, 1))
+
+gradient = get_gradient_2d(0,  255, cols, rows)
+gradient = np.uint8(gradient)
+
+mona_final = (image / gradient) 
+
+cv2.imwrite('mona_final.jpg', mona_final)
+cv2.imshow('out', mona_final) 
 cv2.waitKey()
